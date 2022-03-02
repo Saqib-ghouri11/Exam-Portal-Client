b7281da71529477a019deeda0eaabba2fbf05aef
Saqib Ali Khan <arbaas.khan11@gmail.com>
2022-03-02 13:20:30 +0500

    latest

--- src/app/pages/admin/update-quiz/update-quiz.component.ts
+++ src/app/pages/admin/update-quiz/update-quiz.component.ts
@@ -16,77 +16,77 @@ export class UpdateQuizComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private categoryService: CategoryService,
-    private quizService:QuizService,
-    private _router:ActivatedRoute,
-    private _quizService:QuizService,
+    private quizService: QuizService,
+    private _router: ActivatedRoute,
+    private _quizService: QuizService
   ) {
     this.data = {} as any;
     this.categories = [] as Array<Category>;
     this.data = localStorage.getItem('quizToUpdate');
-    this.quiz = {} as Quiz;
-    this.quizUpdateForm = {} as FormGroup;
   }
+
+
 
   data: any;
 
-  quiz: Quiz;
+  quiz: Quiz = {} as Quiz;
 
-  urlId=0;
+  urlId = 0;
 
   categories: Array<Category>;
-  quizUpdateForm: FormGroup;
+  quizUpdateForm: FormGroup =this.fb.group({
+    id: ['', [Validators.required]],
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    maxMarks: ['', [Validators.required]],
+    numberOfQuestions: ['', [Validators.required]],
+    active: [''],
+    category: this.fb.group({
+      id: ['', [Validators.required]],
+      title: [''],
+      description: [''],
+    }),
+  });
 
   updateQuiz() {
     console.log(
       'here in update quiz ' + JSON.stringify(this.quizUpdateForm.value)
     );
     this.quizService.updateQuiz(this.quizUpdateForm.value).subscribe(
-      (response:any)=>{
-        Swal.fire(
-          "Success!",
-          ""+response.message,
-          "success"
-        );
+      (response: any) => {
+        Swal.fire('Success!', '' + response.message, 'success');
       },
-      (error:any)=>{
-        Swal.fire(
-          "Error!",
-          ""+error.error.message,
-          "error"
-        );
+      (error: any) => {
+        Swal.fire('Error!', '' + error.error.message, 'error');
       }
-
-      );
+    );
   }
 
+
+
   ngOnInit(): void {
-    this.urlId=this._router.snapshot.params['id'];
-   this._quizService.getQuizById(this.urlId).subscribe((response:any)=>{
-      this.quiz=response;
-      console.log(this.quiz);
+    this.urlId = this._router.snapshot.params['id'];
 
-    },(error)=>{
-      console.log(error);
-
-    });
-
-    this.quizUpdateForm = this.fb.group({
-      id:[this.quiz.id,[Validators.required]],
-      title: [this.quiz.title, [Validators.required]],
-      description: [this.quiz.description, [Validators.required]],
-      maxMarks: [this.quiz.maxMarks, [Validators.required]],
-      numberOfQuestions: [this.quiz.numberOfQuestions, [Validators.required]],
-      active: [this.quiz.active],
-      category: this.fb.group({
-        id:['',[Validators.required]],
-        title:[''],
-        description:['']
-      }),
-    });
+    this._quizService.getQuizById(this.urlId).subscribe(
+      (response: any) => {
+        this.quiz = response;
+        this.quizUpdateForm.get('title')?.setValue( this.quiz.title);
+        this.quizUpdateForm.get('description')?.setValue( this.quiz.description);
+        this.quizUpdateForm.get('maxMarks')?.setValue( this.quiz.maxMarks);
+        this.quizUpdateForm.get('numberOfQuestions')?.setValue( this.quiz.numberOfQuestions);
+        this.quizUpdateForm.get('active')?.setValue( this.quiz.active);
+        this.quizUpdateForm.get('category.id')?.setValue( this.quiz.category.id);
+        console.log(this.quiz);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
     this.categoryService.getCategories().subscribe(
       (response: any) => {
         this.categories = response;
+
       },
       (error) => {
         Swal.fire(
@@ -96,5 +96,8 @@ export class UpdateQuizComponent implements OnInit {
         );
       }
     );
+
+
+
   }
 }
